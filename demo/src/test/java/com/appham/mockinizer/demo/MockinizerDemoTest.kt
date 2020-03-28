56481ea74c0e68369ece8f42db865724124eb7fb
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2020-03-28 19:36:13 +0000

    add dummy test

--- demo/src/test/java/com/appham/mockinizer/demo/MockinizerDemoTest.kt
+++ demo/src/test/java/com/appham/mockinizer/demo/MockinizerDemoTest.kt
@@ -1,6 +1,12 @@
 package com.appham.mockinizer.demo
 
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
 class MockinizerDemoTest {
 
-    //TODO
+    @Test
+    fun testDummy() {
+        assertEquals(1, 1)
+    }
 }
