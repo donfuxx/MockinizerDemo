6c07482a7be1714fd86278b037fd2cce600cb524
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2020-03-29 15:38:32 +0100

    add MainViewModelTest

--- demo/src/main/java/com/appham/mockinizer/demo/MainViewModel.kt
+++ demo/src/main/java/com/appham/mockinizer/demo/MainViewModel.kt
@@ -1,7 +1,6 @@
 package com.appham.mockinizer.demo
 
 import androidx.lifecycle.ViewModel
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
@@ -18,7 +17,6 @@ class MainViewModel(
 
         demoRepository.getMockedError()
             .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
             .onErrorReturn { emptyList() }
             .delay(2, TimeUnit.SECONDS)
             .subscribe()
